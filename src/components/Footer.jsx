a92fc61404eb25bf8f8b00526d4f8fdc26335f6e
matiaschanquia <matiaschanquianahuel@gmail.com>
2022-12-23 20:51:20 -0300

    rel="noreferrer" en links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer(props) {
     return (
         <footer className={style.footer}>
             <div className={style.redes}>
-                <a href="https://www.linkedin.com/in/matias-chanquia/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="logo linkedin"/></a>
-                <a href="https://github.com/matiaschanquia" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo github"/></a>
+                <a href="https://www.linkedin.com/in/matias-chanquia/" target="_blank" rel="noreferrer"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="logo linkedin"/></a>
+                <a href="https://github.com/matiaschanquia" target="_blank" rel="noreferrer"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo github"/></a>
             </div>
             <p>&copy; Copyright Simpsons API - Matias Chanquia Dev</p>
         </footer>
